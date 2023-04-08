4b56909ef3e3d7011782db2f7d939640d8b58dbe
OleksandrGnatiuk <oleksandr.gnatiuk@gmail.com>
2023-04-08 23:28:25 +0300

    fixed bugs with @cache

--- MongoDB/search_quote.py
+++ MongoDB/search_quote.py
@@ -1,51 +1,67 @@
 from models import Author, Quote
 import redis
 from redis_lru import RedisLRU
-
+from mongoengine import DoesNotExist
 
 client = redis.StrictRedis(host="localhost", port=6379, password=None)
 cache = RedisLRU(client)
 
 
-def main():
+@cache
+def find_quoter_by_name(value):
+    try:
+        author = Author.objects(fullname__startswith=value.title())[0]
+        quotes = Quote.objects(author=author)
+        if quotes:
+            result = []
+            for quote in quotes:
+                r = f"{quote.quote}\n{quote.author.fullname}     tags:{', '.join(quote.tags)}"
+                result.append(r)
+            return result
+    except DoesNotExist:
+        print(f'Quotes by {value} is not exists')
 
+
+@cache
+def find_quoter_by_tag(value):
+    try:
+        result = []
+        for quote in Quote.objects(tags__startswith=value):
+            r = f"{quote.quote}\n{quote.author.fullname}     tags: {', '.join(quote.tags)}"
+            result.append(r)
+        return result
+    except DoesNotExist:
+        print(f'Quotes with tag:{value} is not exists')
+
+
+def main():
     while True:
         user_command = input("Enter command: ").strip().lower()
         if user_command == "exit":
+            print('Good bye!')
             break
         else:
             try:
                 command, value = user_command.split(":")
                 command, value = command.strip(), value.strip()
-                client.set(command, value)
 
                 if command == "name":
-                    quote_list = []
-                    value = client.get(command).decode()
-                    uuid = Author.objects(fullname__startswith=value.title())[0]
-                    quotes = Quote.objects(author=uuid)
-                    if quotes:
-                        for quote in quotes:
-                            quote_list.append(quote.quote)
-                    print(*quote_list, sep="\n")
+                    print(*find_quoter_by_name(value), sep='\n')
 
                 elif command == "tag":
-                    quote_list = []
-                    value = client.get(command).decode()
-                    for quote in Quote.objects(tags__startswith=value):
-                        quote_list.append(quote.quote)
-                    print(*quote_list, sep="\n")
+                    print(*find_quoter_by_tag(value), sep='\n')
 
                 elif command == "tags":
-                    quote_list = []
-                    value = client.get(command).decode()
-                    for quote in Quote.objects(tags__in=value.split(",")):
-                        quote_list.append(quote.quote)
-                    print(*quote_list, sep="\n")
+                    result = []
+                    for quote in Quote.objects():
+                        for tag in quote.tags:
+                            if tag in value.split(","):
+                                r = f"{quote.quote}\n{quote.author.fullname}      tags: {', '.join(quote.tags)}"
+                                if r not in result:
+                                    result.append(r)
+                    [print(r) for r in result]
                 else:
                     print("Your command is wrong. Please, try again!")
-
-                
             except Exception as err:
                 print(err)
 
